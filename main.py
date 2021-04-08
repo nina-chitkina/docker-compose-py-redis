f37452c2632718dcb3f20b94c3d61abd4c30b262
nina-chitkina <81327073+nina-chitkina@users.noreply.github.com>
2021-04-08 17:21:55 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import redis
 
 r = redis.Redis(host='localhost' , port='6379')
 
+
+# exceptions added by Levan
 try:
     r.set('A' , '5')
     print("set complete")
@@ -9,7 +11,7 @@ except Exception as e:
     print(e)
     print("fail")
 
-
+# exceptions added by Levan
 
 try:
     r.get('A')
